8c6b5be71321416520102cf81d492647c059bd6c
paulpaul168 <github@paulhoeller.at>
2025-04-17 23:14:37 +0200

    Use python3 instead of legacy python

--- run_on_sl1680.sh
+++ run_on_sl1680.sh
@@ -2,7 +2,7 @@
 
 # Start the backend server
 cd backend
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 uvicorn main:app --reload --host 0.0.0.0 --port 8010 &
